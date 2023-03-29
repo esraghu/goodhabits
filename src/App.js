0446e42dcd15dedbae7588de080e23f930204f15
Raghunandan Srinivasan <esraghu@gmail.com>
2023-03-29 19:21:52 +0530

    Updated App.js to include the table element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+function tracker() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Habit Name</th>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+        </tr>
+      </thead>
+      <tbody id="attendanceTableBody">
+      </tbody>
+    </table>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Habit Tracker
+        </h1>
+        <div className="tracker">
+          <tracker />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
